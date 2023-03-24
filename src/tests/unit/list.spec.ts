3f456ea1f214c1717f27491cca735ab880cf8939
Ziya <ziyakerimli04@gmail.com>
2023-03-24 13:05:42 +0400

    create first test for list entity

--- src/tests/unit/list.spec.ts
+++ src/tests/unit/list.spec.ts
@@ -1,9 +1,15 @@
 import { List } from '../../list';
-const list = new List<string>();
-list.append('1');
-list.append('1');
-list.append('2');
-list.append('1');
-list.append('3');
-const res = list.findFirst('1');
-console.log(res);
+
+describe('List', () => {
+  let list: List<number>;
+  beforeEach(() => {
+    list = new List<number>();
+  });
+  describe('#length', () => {
+    it('should return the length of current list', () => {
+      list.append(1);
+      list.append(2);
+      expect(list.length()).toBe(2);
+    });
+  });
+});
